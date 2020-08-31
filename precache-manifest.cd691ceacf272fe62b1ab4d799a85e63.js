4c9773be7c8bf47e2cc0dae8f3522350e3623e2a
jiwon <wldnjs5818@naver.com>
2020-08-31 23:44:03 +0900

    Updates

---
+++ precache-manifest.cd691ceacf272fe62b1ab4d799a85e63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eae41d3736029afc5a9f4919d2d175d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "42c8e9d48f2246881583",
+    "url": "/movie_app/static/css/main.47afaf7d.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c8e9d48f2246881583",
+    "url": "/movie_app/static/js/main.8886cf87.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
